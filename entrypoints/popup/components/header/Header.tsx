7e287fe29d94fd573075b10cc477ef341d12cf0c
DKSolo <dbm95mail@gmail.com>
2025-11-04 21:48:18 +0500

    feat: extension kill switch stylized

--- entrypoints/popup/components/header/Header.tsx
+++ entrypoints/popup/components/header/Header.tsx
@@ -1,10 +1,12 @@
 import "@/assets/tailwind.css";
+import KillSwitch from "../killSwitch/KillSwitch";
 
 export default function Header() {
     return (
-        <div className="w-[424px] h-[48px] flex">
+        <div className="w-[424px] h-[48px] flex justify-around">
             <div className="w-[48px] h-[48px] text-4xl">👁️</div>
             <h1 className='text-2xl'>YaForms accessibility</h1>
+            <KillSwitch />
         </div>
     )
 }
--- entrypoints/popup/components/popup/Popup.tsx
+++ entrypoints/popup/components/popup/Popup.tsx
@@ -4,7 +4,7 @@ import Header from "../header/Header";
 
 export default function Popup() {
     return (
-        <div className="w-[500px] h-[300px] p-6 flex flex-col gap-6">
+        <div className="w-[500px] h-[300px] p-6 flex flex-col gap-6 rounded-5xl">
             <Header />
             <FancyToggle />
             <FancyToggle />
