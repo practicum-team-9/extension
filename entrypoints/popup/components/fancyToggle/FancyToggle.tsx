4ca9faf84133f2b9d41a2ed04e2c4b8de5499a34
DKSolo <dbm95mail@gmail.com>
2025-11-04 22:14:40 +0500

    Feat Rudementary Messaging system

--- entrypoints/popup/components/fancyToggle/FancyToggle.tsx
+++ entrypoints/popup/components/fancyToggle/FancyToggle.tsx
@@ -1,6 +1,20 @@
 import "@/assets/tailwind.css";
 
-export default function FancyToggle() {
+// TO DO: 
+// REPLACE EMOJIs WITH IMGs
+// Replace alerts with messaging to the shadow DOM
+
+
+interface fancyToggleProps {
+    isOnEmoji: string;
+    isOnText: string;
+    isOffEmoji: string;
+    isOffText: string;
+    isOnMessage: string;
+    isOffMessage: string;
+}
+
+export default function FancyToggle(props: fancyToggleProps) {
     const [isOn, setIsOn] = useState(true)
     const toggleOnStyle = "bg-blue-500 w-[50%] h-full rounded-2xl flex items-center p-2 gap-2"
     const toggleOffStyle = "bg-gray-500 w-[50%] h-full rounded-2xl flex items-center p-2 gap-2"
@@ -9,14 +23,15 @@ export default function FancyToggle() {
     return (
         <div className="bg-gray-500 flex w-[364px] h-[64px] justify-between items-center rounded-2xl" onClick={() => {
             isOn ? setIsOn(false) : setIsOn(true);
+            isOn ? alert(props.isOffMessage) : alert(props.isOnMessage);
         }}>
             <div className={isOn ? toggleOnStyle : toggleOffStyle}>
-                <div className="w-[48px] h-[48px] text-4xl bg-white rounded-full">🔊</div>
-                <div className="text-sm">Включить</div>
+                <div className="w-[48px] h-[48px] text-4xl bg-white rounded-full text-center justify-center">{props.isOnEmoji}</div>
+                <div className="text-sm">{props.isOnText}</div>
             </div>
             <div className={isOn ? toggleOffStyle : toggleOnStyle}>
-                <div className="w-[48px] h-[48px] text-4xl bg-white rounded-full">🔈</div>
-                <div className="text-sm">Выключить</div>
+                <div className="w-[48px] h-[48px] text-4xl bg-white rounded-full text-center justify-center">{props.isOffEmoji}</div>
+                <div className="text-sm">{props.isOffText}</div>
             </div>
         </div>
     )
--- entrypoints/popup/components/popup/Popup.tsx
+++ entrypoints/popup/components/popup/Popup.tsx
@@ -6,8 +6,8 @@ export default function Popup() {
     return (
         <div className="w-[500px] h-[300px] p-6 flex flex-col gap-6 rounded-5xl">
             <Header />
-            <FancyToggle />
-            <FancyToggle />
+            <FancyToggle isOnEmoji={"🔊"} isOnText={"Включить"} isOffEmoji={"🔈"} isOffText={"Выключить"} isOnMessage={"soundOn"} isOffMessage={"soundOff"}  />
+            <FancyToggle isOnEmoji={"☀️"} isOnText={"Светлая тема"} isOffEmoji={"🌙"} isOffText={"Темная тема"} isOnMessage={"lightTheme"} isOffMessage={"darkTheme"} />
         </div>
     )
 }
