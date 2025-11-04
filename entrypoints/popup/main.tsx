f42726c4f434f5954195f0b163438900631c3f35
DKSolo <dbm95mail@gmail.com>
2025-11-04 19:45:51 +0500

    TailwindCSS setup

--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+//import App from './App.tsx';
 import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <h1 className='text-3xl font-bold underline text-red-700'>WXT, REACT and TAILWIND are READY!</h1>
   </React.StrictMode>,
 );
