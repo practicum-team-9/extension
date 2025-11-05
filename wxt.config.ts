f0639972f594e667da0726409787df1bb8fecf1c
DKSolo <dbm95mail@gmail.com>
2025-11-05 08:32:39 +0500

    ReadME

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,6 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+  extensionApi: "chrome",
+  manifest: ({ browser, manifestVersion, mode, command }) => {
+    return {
+      manifest_version: 2,
+      name: "YaForms Accessibility",
+      description: "Extension to help visually impaired with Ya Forms",
+      version: "1.0.0",
+      permissions: ["storage", "tabs"]
+    }
+  },
   vite: (() => ({
     plugins: [
       tailwindcss(),
