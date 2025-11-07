b3ef5a38b3d4c73b67d2b823eed4c3803b570f04
DKSolo <dbm95mail@gmail.com>
2025-11-07 10:08:38 +0500

    fix: Extra menu options purged

--- entrypoints/background/index.ts
+++ entrypoints/background/index.ts
@@ -3,43 +3,12 @@ export default defineBackground({
     console.log('Hello background!', { id: browser.runtime.id });
     chrome.runtime.onInstalled.addListener(() => {
       console.log("Chrome runtime ON INSTALLED")
-      chrome.contextMenus.create({
-        id: "makeAccess",
-        title: "Make form Accessible",
-        contexts: ["all"],
-      });
 
       chrome.contextMenus.create({
         id: "newForm",
         title: "Making the new form accessible",
         contexts: ["all"],
       });
-
-      chrome.tabs.onUpdated.addListener((tabId, tab) => {
-          console.log(tab)
-          chrome.tabs.sendMessage(tabId, {
-              type: "autoNewForm"
-          })
-          .then((res) => {
-              console.log("Message recieved!")
-          })
-          .catch((err) => {
-              console.log(err)
-          })
-      })
-
-      chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-        if (info.menuItemId === "makeAccess") {
-          console.log(tab?.id)
-          chrome.tabs.sendMessage(
-            tab?.id!,
-            { action: "makeAccess" },
-            function (response) {
-              console.info("Response is:", response)
-            }
-          );
-        }
-      })
 
       chrome.contextMenus.onClicked.addListener(async (info, tab) => {
         if (info.menuItemId === "newForm") {
--- entrypoints/content/index.tsx
+++ entrypoints/content/index.tsx
@@ -13,7 +13,7 @@ export default defineContentScript({
     console.log('Hello content.');
 
     window.onload = async () => {
-          const ui = await createIUI(ctx, "message")
+          const ui = await createShadowUI(ctx, "message")
           ui.mount()
     } 
 
@@ -23,19 +23,19 @@ export default defineContentScript({
       // if (message.action === "newForm") {
       // }
       switch (message.action) {
-        case "makeAccess":
-          console.log("Make Access action recieved")
-          sendResponse({ status: "Make access action handled"})
-          const ui = await createUi(ctx, "message")
-          ui.mount()
-          break;
+        // case "promtUseModal":
+        //   console.log("Promt to use modal action recieved")
+        //   sendResponse({ status: "Make access action handled"})
+        //   const ui = await createShadowUI(ctx, "message")
+        //   ui.mount()
+        //   break;
 
-        case "autoNewForm":
-          console.log("autoNewForm action recieved")
-          sendResponse({ status: "New Form action handled"})
-          const formUi = await createIUI(ctx, "New Form ")
-          formUi.mount()
-          break;
+        // case "autoNewForm":
+        //   console.log("autoNewForm action recieved")
+        //   sendResponse({ status: "New Form action handled"})
+        //   const formUi = await createIUI(ctx, "New Form ")
+        //   formUi.mount()
+        //   break;
 
         case "newForm":
           console.log("NewForm action recieved")
@@ -51,7 +51,7 @@ export default defineContentScript({
 });
 
 
-const createUi = async (ctx: any, message: string) => {
+const createShadowUI = async (ctx: any, message: string) => {
   return createShadowRootUi(ctx, {
       name: "make-access",
       position: "inline",
@@ -62,9 +62,14 @@ const createUi = async (ctx: any, message: string) => {
         root.render(
           <React.StrictMode>
             <div className="visible absolute top-0 bottom-0 right-0 left-0 z-50 h-full w-full flex justify-center items-center bg-gray-900/50 ">
-              <h1>YaForms Accessibility</h1>
-              <h2>Hello World!</h2>
-              <p>{message}</p>
+              <div className="bg-white rounded-3xl flex flex-col p-6 justify-center items-center w-2xl h-100 gap-4">
+                <h1 className="text-5xl">YaForms Accessibility</h1>
+                <button onClick={() => {
+                  console.log('Button clicked!')
+                  newFormLoaded()
+                }} className="text-3xl bg-black text-white pt-2 pb-2 pl-4 pr-4 text-center justify-center rounded-2xl border border-black">Начать</button>
+                <button onClick={() => {root?.unmount }} className="text-3xl bg-white text-black pt-2 pb-2 pl-4 pr-4 text-center justify-center rounded-2xl border border-black">Выключить расширение</button>
+              </div>
             </div>
           </React.StrictMode>
         );
@@ -78,26 +83,26 @@ const createUi = async (ctx: any, message: string) => {
 }
 
 
-const createIUI = async (ctx: any, message: string) => {
-  return createIntegratedUi(ctx, {
-      position: "inline",
-      anchor: "body",
-      onMount: (uiContainer) => {
-        const root = ReactDOM.createRoot(uiContainer);
-        root.render(
-          <React.StrictMode>
-            <div className="visible absolute top-0 bottom-0 right-0 left-0 z-50 h-full w-full flex justify-center items-center bg-gray-900/50 ">
-              <h1>YaForms Accessibility</h1>
-              <h2>Hello World!</h2>
-              <p>{message}</p>
-            </div>
-          </React.StrictMode>
-        );
-        return root;
-      },
-      onRemove(root) {
-        console.log("Unmounting")
-        root?.unmount();
-      }
-    });
-}
+// const createIUI = async (ctx: any, message: string) => {
+//   return createIntegratedUi(ctx, {
+//       position: "inline",
+//       anchor: "body",
+//       onMount: (uiContainer) => {
+//         const root = ReactDOM.createRoot(uiContainer);
+//         root.render(
+//           <React.StrictMode>
+//             <div className="visible absolute top-0 bottom-0 right-0 left-0 z-50 h-full w-full flex justify-center items-center bg-gray-900/50 ">
+//               <h1>YaForms Accessibility</h1>
+//               <h2>Hello World!</h2>
+//               <p>{message}</p>
+//             </div>
+//           </React.StrictMode>
+//         );
+//         return root;
+//       },
+//       onRemove(root) {
+//         console.log("Unmounting")
+//         root?.unmount();
+//       }
+//     });
+// }
