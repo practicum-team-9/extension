2812e0137e84d52119bc58e2991eda7751d8c7f0
DKSolo <dbm95mail@gmail.com>
2025-11-07 10:44:46 +0500

    feat: Modal initializes work with extension

--- entrypoints/content/index.tsx
+++ entrypoints/content/index.tsx
@@ -2,6 +2,8 @@ import "../popup/style.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { newFormLoaded } from "./scripts/script";
+import Modal from "./modal/Modal";
+import App from "./app/App";
 
 export default defineContentScript({
   matches: ['*://*/*'],
@@ -20,22 +22,7 @@ export default defineContentScript({
     chrome.runtime.onMessage.addListener(
       async (message, sender, sendResponse) => {
       console.log("Recieved action in content script", message)
-      // if (message.action === "newForm") {
-      // }
       switch (message.action) {
-        // case "promtUseModal":
-        //   console.log("Promt to use modal action recieved")
-        //   sendResponse({ status: "Make access action handled"})
-        //   const ui = await createShadowUI(ctx, "message")
-        //   ui.mount()
-        //   break;
-
-        // case "autoNewForm":
-        //   console.log("autoNewForm action recieved")
-        //   sendResponse({ status: "New Form action handled"})
-        //   const formUi = await createIUI(ctx, "New Form ")
-        //   formUi.mount()
-        //   break;
 
         case "newForm":
           console.log("NewForm action recieved")
@@ -61,16 +48,7 @@ const createShadowUI = async (ctx: any, message: string) => {
         const root = ReactDOM.createRoot(app);
         root.render(
           <React.StrictMode>
-            <div className="visible absolute top-0 bottom-0 right-0 left-0 z-50 h-full w-full flex justify-center items-center bg-gray-900/50 ">
-              <div className="bg-white rounded-3xl flex flex-col p-6 justify-center items-center w-2xl h-100 gap-4">
-                <h1 className="text-5xl">YaForms Accessibility</h1>
-                <button onClick={() => {
-                  console.log('Button clicked!')
-                  newFormLoaded()
-                }} className="text-3xl bg-black text-white pt-2 pb-2 pl-4 pr-4 text-center justify-center rounded-2xl border border-black">Начать</button>
-                <button onClick={() => {root?.unmount }} className="text-3xl bg-white text-black pt-2 pb-2 pl-4 pr-4 text-center justify-center rounded-2xl border border-black">Выключить расширение</button>
-              </div>
-            </div>
+            <App />
           </React.StrictMode>
         );
         return root;
