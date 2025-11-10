0b2b5fa9939fd35782b132c0ac5888220f168e9c
DKSolo <dbm95mail@gmail.com>
2025-11-10 09:55:47 +0500

    feat: Formatting of data implemented

--- entrypoints/content/app/screens/shadowForm/ShadowForm.tsx
+++ entrypoints/content/app/screens/shadowForm/ShadowForm.tsx
@@ -5,13 +5,48 @@ import CommonButton from "../../components/buttons/CommonButton";
 import ShadowQuestion from "./shadowQuestion/ShadowQuestion";
 import { sayTheThing } from "@/entrypoints/content/scripts/speechScripts/sayTheThing";
 import { useThingsToSay } from "@/entrypoints/hooks/useSettingsData/useThingsToSay";
+import { HTMLInputTypeAttribute } from "react";
 
 interface iShadowFormProps {
     shadowFormData: iShadowFormData
 }
 
+// To submit the form
 interface iSubmitAnswers {
     [key: string] : string[],
+}
+
+
+// Formatting data to work with
+export interface iShadowFormPageItemsFormatted {
+    hidden: boolean,
+    id: string,
+    label: string,
+    multiline: boolean,
+    type: string,
+    widget?: boolean,
+    validations?: {type: string}[],
+    validationArray: string[],
+    questionType: HTMLInputTypeAttribute,
+    speech: string
+}
+
+export interface iShadowFormPagesFormatted {
+    items: iShadowFormPageItemsFormatted[]
+}
+
+export interface iShadowFormFormatted {
+    footer: boolean,
+    id: string,
+    iFrame: boolean,
+    name: string,
+    pages: iShadowFormPagesFormatted[],
+    teaser: boolean,
+    texts?: {
+        submit?: string,
+        back?: string,
+        next?: string
+    }
 }
 
 export default function ShadowForm(props: iShadowFormProps) {
@@ -19,7 +54,27 @@ export default function ShadowForm(props: iShadowFormProps) {
     const [ pageNumber, setPageNumber ] = useState(0)
     const [ isValid, setIsValid ] = useState(true)
     const [ formState, setFormState ] = useState<iSubmitAnswers>()
-    // const { thingsToSay } = useThingsToSay();
+    const [ formattedData, setFormattedData ] = useState<iShadowFormFormatted>({
+        footer: true,
+        id: 'Загружаем...',
+        iFrame: false,
+        name: 'Загружаем...',
+        pages: [
+            {
+               items: [{
+                    hidden: false,
+                    id: 'Загружаем...',
+                    label: 'Загружаем...',
+                    multiline: false,
+                    type: 'Загружаем...',
+                    validationArray: [],
+                    questionType: 'text',
+                    speech: 'Загружаем...'
+                }]
+            }
+        ],
+        teaser: true,
+    })
 
     const nextQuestion = () => {
         const maxPages = props.shadowFormData.pages.length
@@ -58,14 +113,73 @@ export default function ShadowForm(props: iShadowFormProps) {
         }
     }
 
+    useEffect(() => {
+        const { pages } = props.shadowFormData
+        var itemsArray: iShadowFormPageItemsFormatted[]
+        var pagesArray: iShadowFormPagesFormatted[] = []
+        var questionType: HTMLInputTypeAttribute
+        var validationsArray: string[]
+        var speech: string
+
+
+        pages.forEach((page) => {
+            itemsArray = []
+            page.items.forEach((item) => {
+                validationsArray = []
+                speech = ''
+                item.validations?.forEach((validation) => {
+                    validationsArray.push(validation.type)
+
+                })
+                if (validationsArray.includes('required')) {
+                    speech += 'Это обязательный вопрос.'
+                }
+                if (item.type === 'date') {
+                    speech += 'Введите дату в формате День Месяц Год.'
+                    questionType = 'date'
+                } else if (item.type === 'enum') {
+                    speech += 'Выберите один вариант из предложенных.'
+                    questionType = 'dropdown'
+                } else if (item.type === 'boolean') {
+                    speech += 'Нажмите поле чтобы подтвердить или снять подтверждение.'
+                    questionType = 'checkbox'
+                } 
+                else if (item.type === 'string' && validationsArray.includes('email')) {
+                    speech += 'Введите электронную почту.'
+                    questionType = 'email'
+                } 
+                else if (item.type === 'string' && validationsArray.includes('phone')) {  
+                    speech += 'Введите номер телефона.'
+                    questionType = 'tel'
+                } 
+                else if (item.type === 'string') {
+                    speech += 'Введите текст в поле.'
+                    questionType = 'text'
+                }    
+                itemsArray.push({
+                    ...item,
+                    validationArray: validationsArray,
+                    questionType: questionType,
+                    speech: speech
+                })
+            })
+            pagesArray.push({items: itemsArray})            
+        })
+
+        setFormattedData({
+            ...props.shadowFormData,
+            pages: pagesArray
+        })
+    }, [props.shadowFormData])
+
     return (
         <div className="flex flex-col items-center ">
             <h1 className="text-5xl">{props.shadowFormData.name}</h1>
             <div className="w-3xl h-100 border border-[#E5E5E5] rounded-3xl flex flex-col p-6 justify-between">
-                <ShadowQuestion shadowQuestionData={props.shadowFormData.pages[pageNumber].items[questionNumber]}/>
+                <ShadowQuestion shadowQuestionData={formattedData.pages[pageNumber].items[questionNumber]}/>
                 <div className="flex flex-row justify-between">
                     <CommonButton onClick={previousQuestion} text={"Назад"} />
-                    <CommonButton onClick={() => sayTheThing('Thing')} text={"Повторить"} />
+                    <CommonButton onClick={() => sayTheThing(formattedData.pages[pageNumber].items[questionNumber].speech)} text={"Повторить"} />
                     <AccentButton disabled={!isValid} onClick={nextQuestion} text={"Вперед"} />
                 </div>
                 <div className="flex flex-row justify-between h-8 text-[#26262699]">
--- entrypoints/content/app/screens/shadowForm/shadowQuestion/ShadowQuestion.tsx
+++ entrypoints/content/app/screens/shadowForm/shadowQuestion/ShadowQuestion.tsx
@@ -3,72 +3,60 @@ import { iShadowFormPageItemsData } from "../../../App";
 import { Dispatch, HTMLInputTypeAttribute, SetStateAction } from "react";
 import { useThingsToSay } from "@/entrypoints/hooks/useSettingsData/useThingsToSay";
 import { sayTheThing } from "@/entrypoints/content/scripts/speechScripts/sayTheThing";
+import { iShadowFormPageItemsFormatted } from "../ShadowForm";
 
 interface iShadowQuestionProps {
-    shadowQuestionData: iShadowFormPageItemsData
+    shadowQuestionData: iShadowFormPageItemsFormatted
 }
 
+
 export default function ShadowQuestion(props: iShadowQuestionProps) {
-    const { hidden, id, label, multiline, type, widget, validations } = props.shadowQuestionData
-    const [ questionType, setQuestionType ] = useState<HTMLInputTypeAttribute>('text')
-    //const {} = useThingsToSay();
+    const { hidden, id, label, multiline, type, widget, validations, validationArray, questionType, speech  } = props.shadowQuestionData
 
-    var validationsArray: string[] = []
-        
-    validations?.forEach((validation) => {
-        validationsArray.push(validation.type)
-    })
 
-    if (type === 'date') {
-        sayTheThing('Question type date.')
+    if (questionType === 'date') {
         return (
             <>
                 <h2 className="text-3xl">{label}</h2>
-                <input name={id} id={id} type='date' required={validationsArray.includes('required') } className="text-3xl w-full rounded-md border-2 border-[#E5E5E5] pt-4 pb-4 pl-2 pr-2"/>
+                <input name={id} id={id} type='date' required={validationArray.includes('required') } className="text-3xl w-full rounded-md border-2 border-[#E5E5E5] pt-4 pb-4 pl-2 pr-2"/>
             </>
         )
-    } else if (type === 'enum') {
-        sayTheThing('Question type drop.')
+    } else if (questionType === 'dropdown') {
         return (
             <>
                 <h2 className="text-3xl">{label}</h2>
-                <input name={id} id={id} type='dropdown' required={validationsArray.includes('required') } className="text-3xl w-full rounded-md border-2 border-[#E5E5E5] p-2"/>
+                <input name={id} id={id} type='dropdown' required={validationArray.includes('required') } className="text-3xl w-full rounded-md border-2 border-[#E5E5E5] p-2"/>
             </>
         )
-    } else if (type === 'boolean') {
-        sayTheThing('Question type Checkbox.')
+    } else if (questionType === 'checkbox') {
         return (
             <div className="flex flex-row w-full rounded-md border-2 border-[#E5E5E5] pt-4 pb-4 pl-2 pr-2">
-                <input name={id} id={id} type='checkbox' required={validationsArray.includes('required')} className="flex flex-row w-12 h-12 rounded-md pt-4 pb-4 pl-2 pr-2"/>
+                <input name={id} id={id} type='checkbox' required={validationArray.includes('required')} className="flex flex-row w-12 h-12 rounded-md pt-4 pb-4 pl-2 pr-2"/>
                 <h2 className="text-2xl">{label}</h2>
             </div>
         )
     } 
-    else if (type === 'string' && validationsArray.includes('email')) {
-        sayTheThing('Question type email.')
+    else if (questionType === 'email') {
         return (
             <>
                 <h2 className="text-3xl">{label}</h2>
-                <input name={id} id={id} type='email' required={validationsArray.includes('required') } className="text-3xl w-full rounded-md border-2 border-[#E5E5E5] pt-4 pb-4 pl-2 pr-2" />
+                <input name={id} id={id} type='email' required={validationArray.includes('required') } className="text-3xl w-full rounded-md border-2 border-[#E5E5E5] pt-4 pb-4 pl-2 pr-2" />
             </>
         )
     } 
-    else if (type === 'string' && validationsArray.includes('phone')) {
-        
-        sayTheThing('Question type phone.')
+    else if (questionType === 'tel') {
         return (
             <>
                 <h2 className="text-3xl">{label}</h2>
-                <input name={id} id={id} type='tel' required={validationsArray.includes('required') } className="text-3xl w-full rounded-md border-2 border-[#E5E5E5] pt-4 pb-4 pl-2 pr-2" />
+                <input name={id} id={id} type='tel' required={validationArray.includes('required') } className="text-3xl w-full rounded-md border-2 border-[#E5E5E5] pt-4 pb-4 pl-2 pr-2" />
             </>
         )          
     } 
     else if (type === 'string') {
-        sayTheThing('Question type text.')
         return (
             <>
                 <h2 className="text-3xl">{label}</h2>
-                <input name={id} id={id} type='text' required={validationsArray.includes('required') } className="text-3xl w-full rounded-md border-2 border-[#E5E5E5] pt-4 pb-4 pl-2 pr-2" />
+                <input name={id} id={id} type='text' required={validationArray.includes('required') } className="text-3xl w-full rounded-md border-2 border-[#E5E5E5] pt-4 pb-4 pl-2 pr-2" />
             </>
         )
     }    
